d77e040927c5254f31e5950d002342a60532a2d3
betaniagonzalez31@gmail.com <betaniagonzalez31@gmail.com>
2022-09-20 19:51:02 -0300

    sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useEffect, useState } from 'react'
+import './App.scss'
 
-function App() {
+
+
+const Calculator = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='container my-2'>
+        <div className="row">
+          <div className="col-12">
+            <h1 className='display-6 fw-bolder text-center text-primary'>
+              calculator
+            </h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div className='card mb-3 pt-3 shadow ' >
+              <div className="card-body text-primary">
+                <input type="text" className='form-control form-control-lg mb-4 text-center bg-light fs-4 text-primary shadow' value='.' />
+                <div className='row'>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                </div>
+
+                <div className='row mt-2'>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                </div>
+
+                <div className='row mt-2'>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                </div>
+
+                <div className='row mt-2'>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                </div>
+
+                <div className='row mt-2'>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                  <div className="col-3">
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default Calculator
