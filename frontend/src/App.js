9adfcffc3f9af33a0f1601076ec4d58070a70baa
Bruno Martín <bruno.pedraza@conexa.ai>
2022-09-23 22:42:32 -0300

    completed work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,14 +26,11 @@ const Calculator = () => {
 
   return (
     <div className='app'>
-      <div className="title-container">
-        <h1 className='display-6 fw-bolder text-center text-primary'>
+        <h1 className=' text-center text-primary'>
           calculator
         </h1>
-        <hr />
-      </div>
       <div className='calculator-container'>
-        <Pantalla input={input} />
+          <Pantalla input={input} />
           {/* <input type="text" className='form-control form-control-lg mb-4 text-center bg-light fs-4 text-primary shadow' value='' /> */}
           <div className="my-row">
             <Button controlClick={addInput}>7</Button>
@@ -54,14 +51,16 @@ const Calculator = () => {
             <Button controlClick={addInput}>+</Button>
           </div>
           <div className='my-row'>
-            <Button controlClick={calculateResult}>=</Button>
             <Button controlClick={addInput}>0</Button>
             <Button controlClick={addInput}>.</Button>
+            <Button controlClick={calculateResult}>=</Button>
             <Button controlClick={addInput}>/</Button>
-          <div className='my-row'>
-            <ButtonClear controlClickClear={() => setInput('')}> Clear </ButtonClear>
           </div>
-        </div>
+          <div className='my-row'>
+            <ButtonClear controlClickClear={() => setInput('')}> 
+              Clear 
+            </ButtonClear>
+          </div>
       </div>
     </div>                                                                                                                                     
   )
